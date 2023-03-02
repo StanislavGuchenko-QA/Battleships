c7e68182a5526a1ad06641f38330c3ba86014ae1
Stanislav <122748913+StanislavGuchenko-QA@users.noreply.github.com>
2023-03-02 11:59:25 +0300

    Add files via upload

---
+++ Battleships.js
@@ -0,0 +1,30 @@
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Приготовиться, цель, огонь! (Введи число от 0 до 6):");
+    if (guess < 0 || guess > 6) {
+        alert("Введите число от 0 до 6!");
+    } else {
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("Попал!");
+            hits = hits +1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("Корабль потоплен!");
+            }
+        } else {
+            alert("Промах!");
+        }
+    }
+}
+
+var stats = "Ты справился за " + guesses + " попыток). Твоя точность равна " + (3/guesses);
+alert(stats);
